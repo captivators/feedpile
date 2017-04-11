9ccbe5c180bee936a2f94e00ef69238ed1c613f7
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2017-04-11 07:50:25 +0500

    new changes (#45)
    * added articles and users api routes, created cron job
    
    * fixed pakcage.json

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const app = require('./app');
+const cronJob = require('./cronjob');
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+cronJob.start();
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,9 +1,13 @@
 const Feed = require('./models/feed');
+const User = require('./models/user');
+const Article = require('./models/article');
 
+//=======API TEST ROUTE=======
 exports.test = (req, res) => {
   res.json({message: 'successfully received'});
 };
 
+//=======FEED API ROUTES=======
 exports.getAllFeeds = (req, res) => {
   Feed.find(function(err, feeds) {
     if (err) {
@@ -51,3 +55,99 @@ exports.deleteFeed = (req, res) => {
   });
 };
 
+//=======USER API ROUTES=======
+exports.getAllUsers = (req, res) => {
+  User.find(function(err, users) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(users);
+  });
+};
+
+exports.createUser = (req, res) => {
+  var user = new User();      // create a new instance of the user model
+  user.username = req.body.username;
+  user.password = req.body.password;
+
+  // save the user and check for errors
+  user.save(function(err) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json({ status: 201, message: 'User created!' });
+  });
+};
+
+exports.getOneUser = (req, res) => {
+  User.findById(req.params.userId, function(err, user) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(user);
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  User.remove({
+    _id: req.params.userId
+  }, function(err, user) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json({ status: 200, message: 'User deleted!' });
+  });
+};
+
+//=======ARTICLE API ROUTES=======
+exports.getAllArticles = (req, res) => {
+  Article.find(function(err, articles) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(articles);
+  });
+};
+
+exports.createArticle = (req, res) => {
+  var article = new Article();      // create a new instance of the article model
+  article.url = req.body.url;
+  article.datePublished = req.body.datePublished;
+  article.feedId = req.body.feedId;
+
+  // save the article and check for errors
+  article.save(function(err) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json({ status: 201, message: 'Article created!' });
+  });
+};
+
+exports.getOneArticle = (req, res) => {
+  Article.findById(req.params.articleId, function(err, article) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(article);
+  });
+};
+
+exports.deleteArticle = (req, res) => {
+  Article.remove({
+    _id: req.params.articleId
+  }, function(err, user) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json({ status: 200, message: 'Article deleted!' });
+  });
+};
