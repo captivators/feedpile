c666a6c91366cd8894eeb18171dd9bfb4f9789d7
PantherHawk <alexrosenthalster@gmail.com>
2017-04-11 14:56:57 -0400

    added server tests

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -33,12 +33,12 @@ describe('ROUTES', function() {
    });
  });
 
- it('should accept POST requests to /classes/messages', function(done) {
+ it('should accept POST requests to /api/feeds', function(done) {
    var requestParams = {method: 'POST',
-     uri: 'http://127.0.0.1:3000/classes/messages',
+     uri: 'http://127.0.0.1:8080/api/feeds',
      json: {
-       username: 'Jono',
-       message: 'Do my bidding!'}
+       name: 'Pitchfork',
+       url: 'http://pitchfork.com/rss/news/'}
    };
 
    request(requestParams, function(error, response, body) {
@@ -51,24 +51,37 @@ describe('ROUTES', function() {
    var requestParams = {method: 'POST',
      uri: 'http://127.0.0.1:8080/api/feeds',
      json: {
-       username: 'Jono',
-       message: 'Do my bidding!'}
+       name: 'Pitchfork',
+       url: 'http://pitchfork.com/rss/news/'}
    };
 
    request(requestParams, function(error, response, body) {
      // Now if we request the log, that message we posted should be there:
-     request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
-       var messages = JSON.parse(body).results;
-       expect(messages[0].username).to.equal('Jono');
-       expect(messages[0].message).to.equal('Do my bidding!');
+     request('http://127.0.0.1:8080/api/feeds', function(error, response, body) {
+       var feed = JSON.parse(body).results;
+       expect(feeds[0].name).to.equal('Pitchfork');
        done();
      });
    });
  });
 
  it('Should 404 when asked for a nonexistent endpoint', function(done) {
-   request('http://127.0.0.1:3000/arglebargle', function(error, response, body) {
+   request('http://127.0.0.1:8080/skoopidiboopbop', function(error, response, body) {
      expect(response.statusCode).to.equal(404);
+     done();
+   });
+ });
+
+ it('Should get individual feeds when selected', function(done) {
+   var requestParams = {method: 'POST',
+     uri: 'http://127.0.0.1:8080/api/feeds/:' /* what do we use for feedId here?*/,
+     json: {
+       name: 'Pitchfork',
+       url: 'http://pitchfork.com/rss/news/'}
+   };
+
+   request(requestParams, function(error, response, body) {
+     expect(response.statusCode).to.equal(201);
      done();
    });
  });
