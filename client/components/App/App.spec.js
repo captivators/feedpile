119247558d32a73d293d47327f66016860cf0b4c
Faiz Mohammed <faizmohammed178@gmail.com>
2017-04-16 12:21:36 -0700

    resolved merge conflicts

--- client/components/App/App.spec.js
+++ client/components/App/App.spec.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from '../../store';
-import { Unwrapped as UnwrappedApp } from './App'
+import App from './App'
 import { shallow, render } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 
 test('App take a snapshot', () => {
-  const component = shallow(<UnwrappedApp />);
+  const component = shallow(<App />);
   const tree = shallowToJson(component);
   expect(tree).toMatchSnapshot();
 });
 
 test('App should render a Sidebar and ReaderList', () => {
-  const component = render(<Provider store={store}><UnwrappedApp history={{}}/></Provider>);
+  const component = render(<Provider store={store}><App history={{}}/></Provider>);
   expect(component.find('.sidebar-container').length).toEqual(1);
   expect(component.find('.reader-list-container').length).toEqual(1);
 });
