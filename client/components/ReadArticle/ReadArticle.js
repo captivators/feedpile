146794dd2104487b7da31700d2a3a19bef8ad4cf
Darin Allen <darinallen@gmail.com>
2017-04-17 10:57:59 -0400

    Update ReadArticle view

--- client/components/ReadArticle/ReadArticle.js
+++ client/components/ReadArticle/ReadArticle.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
+import DOMPurify from 'dompurify';
 import './ReadArticle.css';
 
 const ReadArticle = (props) => {
   return (
     <div >
       <Link to="/app"><button>back</button></Link>
-      <p><b>Selected article's description</b> {props.selectedArticle.description}</p>
+      <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(props.selectedArticle.description)}}></div>
     </div>
   )
 };
