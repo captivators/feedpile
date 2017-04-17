82ea14bdbe9ff513da3c08843e226f2a4aef19a2
PantherHawk <alexrosenthalster@gmail.com>
2017-04-16 21:00:27 -0400

    designed header with material and put styles in css file

--- client/components/Home/Header/Header.js
+++ client/components/Home/Header/Header.js
@@ -2,8 +2,19 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import AppBar from 'material-ui/AppBar'
-import FlatButton from 'material-ui/FlatButton'
+import AppBar from 'material-ui/AppBar';
+import indigoA400 from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import './Header.css'
+
+const muiTheme = getMuiTheme({
+  pallette: {
+    textColor: indigoA400
+  },
+  appBar: {
+    height: 50,
+  },
+})
 
 const style = {
   root: {
@@ -15,10 +26,11 @@ const style = {
 
 const Header = (props) => {
   return (
-    <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-    <AppBar style={style.root}
+    <MuiThemeProvider muiTheme={muiTheme}>
+    <AppBar className="header"
     title="Feedpile"
     iconElementRight={<FlatButton label="Sign in" />}
+    zDepth={4}
     />
     </MuiThemeProvider>
   )
