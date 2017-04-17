e1db0baefd20b501385f71aace45d84f1d0e16b8
PantherHawk <alexrosenthalster@gmail.com>
2017-04-16 20:54:54 -0400

    designed features with material ui and put feature styles in css file

--- client/components/Home/Feature/Feature.js
+++ client/components/Home/Feature/Feature.js
@@ -1,34 +1,32 @@
 import React from 'react';
+import './Feature.css'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import Paper from 'material-ui/Paper';
-
-const style = {
-  first: {
-    paddingTop: '45px',
-    paddingBottom: '45px',
-    width: '100%',
-    height: '200px',
-    textAlign: 'center',
-    h1: {
-      color: '#fff',
-      marginTop: 0,
-      fontSize: '48px',
-    }
-  }
-}
+import {CardHeader, Card, CardText} from 'material-ui/Card';
+import Amber from 'material-ui/styles/colors'
 
 const Feature = (props) => {
   return (
     <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-    <Paper zDepth={5}>
-    <div style={style.first}>
-      <i className="large material-icon splash-icons"></i>
-      <h5>{props.info.featureName}</h5>
-      <p>{props.info.description}</p>
+    <div className="root">
+    <Card zDepth={5}>
+      <CardHeader
+        title={props.info.name}
+        textStyle={{alignSelf: "center"}}
+        actAsExpander={true}
+        showExpandableButton={true}
+        className="first"
+      />
+      <CardText expandable={true}>
+        <ul>
+        <li>{props.info.featureName}</li>
+        <br/>
+        <li>{props.info.description}</li>
+        </ul>
+      </CardText>
+    </Card>
     </div>
-    </Paper>
     </MuiThemeProvider>
     )
 }
