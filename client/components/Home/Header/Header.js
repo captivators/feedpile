f1f3e14fff398b4dac31919cbbaf2425a580c53f
PantherHawk <alexrosenthalster@gmail.com>
2017-04-19 00:17:42 -0400

    merge authorization details to header

--- client/components/Home/Header/Header.js
+++ client/components/Home/Header/Header.js
@@ -2,28 +2,12 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-<<<<<<< HEAD
-import AppBar from 'material-ui/AppBar';
-import indigoA400 from 'material-ui/styles/colors';
-import FlatButton from 'material-ui/FlatButton';
-import './Header.css'
-
-const muiTheme = getMuiTheme({
-  pallette: {
-    textColor: indigoA400
-  },
-  appBar: {
-    height: 50,
-  },
-})
-
-=======
 import AppBar from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton'
 import { connect } from 'react-redux';
 import { loginSuccess, loginError} from '../../../actions'
 import Auth0Lock from 'auth0-lock';
->>>>>>> 521f4af06c8bad4355de5b5abd44915d4628d68b
+
 const style = {
   root: {
     display: 'flex',
@@ -34,26 +18,6 @@ const style = {
 
 const Header = React.createClass({
 
-<<<<<<< HEAD
-const Header = (props) => {
-  return (
-    <MuiThemeProvider muiTheme={muiTheme}>
-    <AppBar className="header"
-    title="Feedpile"
-    iconElementRight={<FlatButton label="Sign in" />}
-    showMenuIconButton={false}
-    zDepth={4}
-    style={
-      {
-        backgroundColor: "#A9A9A9",
-        color: 'black',
-      }
-    }
-    />
-    </MuiThemeProvider>
-  )
-}
-=======
   componentDidMount() {
     this.lock = new Auth0Lock('V22h9G3v-_1RucHOwP71LqZpQJCSiL6n', 'faizone.auth0.com');
     this.lock.on("authenticated", function(authResult) {
@@ -80,6 +44,5 @@ const Header = (props) => {
     )
   }
 });
->>>>>>> 521f4af06c8bad4355de5b5abd44915d4628d68b
 
 export default connect(null, {loginSuccess, loginError})(Header);
