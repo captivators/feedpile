04ab5f5acd5fff7d66092ee467cdc3d494ad67a8
PantherHawk <alexrosenthalster@gmail.com>
2017-04-20 00:17:32 -0400

    mods to landingpage

--- client/components/Home/Home/Home.spec.js
+++ client/components/Home/Home/Home.spec.js
@@ -6,7 +6,7 @@ import store from '../../../store'
 import { shallow, render } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 
-test('Homepage snapshot test', () => {
+test('Landingpage: snapshot test', () => {
 
   const component = shallow(
     <Home />
@@ -15,7 +15,7 @@ test('Homepage snapshot test', () => {
   expect(tree).toMatchSnapshot();
 });
 
-test ('Homepage should render teammate info, featurelist, and techlist components', () => {
+test ('Landingpage: should render teammate info, featurelist, and techlist components', () => {
   const component = render(<Provider store={store}><Home /></Provider>);
   expect(component.find('.features').length).toEqual(1);
   expect(component.find('.feature-item').length).toEqual(3);
