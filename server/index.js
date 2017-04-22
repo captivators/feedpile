6670a313b1fdb372fd75410a53d1e7d30957c6cd
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2017-04-22 19:38:31 +0000

    changed port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const cronJob = require('./feed-parser/cronjob');
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
