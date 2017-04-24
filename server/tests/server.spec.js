f44d13d823bf8dbb0865cbe2ad62630e5b304aca
PantherHawk <alexrosenthalster@gmail.com>
2017-04-24 17:27:33 -0400

    looking for bug in serverside tests

--- server/tests/server.spec.js
+++ server/tests/server.spec.js
@@ -1,13 +1,18 @@
 var request = require('request');
-require('mocha');
 var expect = require('chai').expect;
-var user = {userId: '12345', url: "http://www.reddit.com/.rss", categoryName: 'some news outlet'};
+var user = {
+  userId: '12345',
+  url: "http://rss.cnn.com/rss/cnn_world.rss",
+  categoryName: 'some news outlet',
+  feedId: '58fb76c741ac481628efaf3a'
+};
 
 // make a user and a category variable to use throughout
 
 describe('API REQUESTS: ', function() {
 
   before((done) => {
+
     const userData = {
       method: 'POST',
       uri: 'http://127.0.0.1:8080/api/users',
@@ -22,14 +27,27 @@ describe('API REQUESTS: ', function() {
           method: 'POST',
           uri: 'http://127.0.0.1:8080/api/feeds',
           json: {
-            url: "http://www.reddit.com/.rss",
+            url: "http://rss.cnn.com/rss/cnn_top.rss",
             userId: "12345",
             categoryId: "58fa4fde51f58c9b7dc8b146",
           },
         }
         request(feedData, (error, response, body) => {
-          user.feedId = body.message.slice(14, 38).trim();
-          done();
+          console.log('35')
+          // user.feedId = body.newObj.feeds[0].feedId;
+          console.log('35', user.feedId)
+          const articlesData = {
+            method: 'POST',
+            uri: 'http://127.0.0.1:8080/api/articles',
+            json: {
+              feedId: user.feedId,
+            }
+          }
+            done();
+          request(articlesData, (error, response, body) => {
+            console.log('47', body[0])
+            user.articleId = body[0]._id; // []
+          })
         })
       })
     })
@@ -38,34 +56,38 @@ describe('API REQUESTS: ', function() {
   describe('FEED API ROUTES', () => {
 
     it ('should get one specific feed', (done) => {
+      console.log('54', user.feedId)
       request('http://127.0.0.1:8080/api/feeds/' + user.feedId, (error, response, body) => {
-        var feed = JSON.parse(body);
-        expect(feed.url).to.equal("http://www.reddit.com/.rss")
+        const feed = JSON.parse(body);
+    console.log('57', feed)
+
+        expect(feed.url).to.equal("http://rss.cnn.com/rss/cnn_world.rss")
         done();
       })
     })
-    it('should delete a feed', (done) => {
-      var requestParams = {
-        method: 'DELETE',
-          uri: 'http://127.0.0.1:8080/api/feeds/' + user.feedId,
-          json: {
-            url: "http://www.reddit.com/.rss",
-            userId: "12345",
-            categoryId: "58fa4fde51f58c9b7dc8b146",
-          },
-      }
-      request(requestParams, (error, response, body) => {
-        request('http://127.0.0.1:8080/api/feeds/', (error, response, body) => {
-          var feeds = JSON.parse(body);
-          var found = feeds.find((feed) => {
-            return feed._id === user.feedId;
-          });
-          console.log(found)
-          expect(found).to.equal(undefined);
-          done();
-        })
-      })
-    })
+
+    // it('should delete a feed', (done) => {
+    //   const requestParams = {
+    //     method: 'DELETE',
+    //       uri: 'http://127.0.0.1:8080/api/feeds/' + user.feedId,
+    //       json: {
+    //         url: "http://rss.cnn.com/rss/cnn_world.rss",
+    //         userId: "12345",
+    //         categoryId: "58fa4fde51f58c9b7dc8b146",
+    //       },
+    //   }
+    //   request(requestParams, (error, response, body) => {
+    //     request('http://127.0.0.1:8080/api/feeds/', (error, response, body) => {
+    //       const feeds = JSON.parse(body);
+    //       const found = feeds.find((feed) => {
+    //         return feed._id === user.feedId;
+    //       });
+    //       console.log(found)
+    //       expect(found).to.equal(undefined);
+    //       done();
+    //     })
+    //   })
+    // })
 
    it('should get all feeds', function(done) {
      request('http://127.0.0.1:8080/api/feeds', function(error, response, body) {
@@ -78,21 +100,20 @@ describe('API REQUESTS: ', function() {
   describe('USER API ROUTES: ', () => {
 
 
-  //   it('should fetch one user', (done) => {
-  //     request('http://127.0.0.1:8080/api/users/' + user.userId, (error, response, body) => {
-  //       var u = JSON.parse(body)
-  //       console.log('line 96', user._id + ' vs ' + u._id);
-  //       expect(u._id).to.equal(user._id);
-  //       done();
-  //     })
-  //   })
-  // })
-// });
+    it('should fetch one user', (done) => {
+      request('http://127.0.0.1:8080/api/users/' + user.userId, (error, response, body) => {
+        var u = JSON.parse(body)
+        console.log('line 96', user._id + ' vs ' + u._id);
+        expect(u._id).to.equal(user._id);
+        done();
+      })
+    })
+
     it('should create a new user', (done) => {
       request('http://127.0.0.1:8080/api/users/', (error, response, body) => {
-        var users = JSON.parse(body);
+        const users = JSON.parse(body);
 
-        var addedUser = users.find((u) => {
+        const addedUser = users.find((u) => {
           return u.userId === user.userId;
         }).userId;
 
@@ -101,64 +122,20 @@ describe('API REQUESTS: ', function() {
       })
     })
 
-    it('should delete a user', (done) => {
-      const requestParams = {
-        method: 'DELETE',
-        uri: 'http://127.0.0.1:8080/api/users/' + user._id,
-      }
+    // it('should delete a user', (done) => {
+    //   const requestParams = {
+    //     method: 'DELETE',
+    //     uri: 'http://127.0.0.1:8080/api/users/' + user._id,
+    //   }
 
-      request(requestParams, (error, response, body) => {
-        var body = JSON.parse(body)
-        var deleteMessage = body.message;
-        expect(deleteMessage).to.equal('User deleted!')
-        done();
-      })
-    })
+    //   request(requestParams, (error, response, body) => {
+    //     body = JSON.parse(body)
+    //     const deleteMessage = body.message;
+    //     expect(deleteMessage).to.equal('User deleted!')
+    //     done();
+    //   })
+    // })
   })
-
-
-//  it('should send back parsable stringified JSON', function(done) {
-//    request('http://127.0.0.1:8080/api/feeds', function(error, response, body) {
-//      expect(JSON.parse.bind(this, body)).to.not.throw();
-//      done();
-//    });
-//  });
-
-//  it('should send back an array', function(done) {
-//    request('http://127.0.0.1:8080/api/feeds', function(error, response, body) {
-//     console.log('sauce')
-//      var parsedBody = JSON.parse(body);
-//      expect(parsedBody).to.be.instanceof(Array);
-//      done();
-//    });
-//  });
-
-//  it('should create a new feed', function(done) {
-//     var requestParams = {
-//       method: 'POST',
-//       uri: 'http://127.0.0.1:8080/api/feeds',
-//       json: {
-//         url: 'http://feeds.bbci.co.uk/news/rss.xml',
-//         userId: '12345',
-//         categoryId: '58fa4fde51f58c9b7dc8b146',
-//       },
-//     };
-
-//    request(requestParams, function(error, response, body) {
-//     console.log('response.statusCode', response.statusCode)
-//      expect(response.statusCode).to.equal(200);
-//      done();
-//    });
-//  });
-
-//  it('should respond with feeds that were previously posted', function(done) {
-//      request('http://127.0.0.1:8080/api/feeds', function(error, response, body) {
-//        var feeds = JSON.parse(body);
-//        expect(feeds[1].name).to.equal('BBC News - Home');
-//        done();
-//      });
-//    });
-//  });
 
   describe('CATEGORY API ROUTES', () => {
 
@@ -192,5 +169,44 @@ describe('API REQUESTS: ', function() {
      });
     });
   })
+
+  describe('ARTICLE API ROUTES', () => {
+    it('should get articles by feed id', (done) => {
+      const requestParams = {
+        method: 'POST',
+        uri: 'http://127.0.0.1:8080/api/articles',
+        json: {
+          feedId: user.feedId,
+        }
+      }
+      request(requestParams, (error, response, body) => {
+        var articles = body;
+        // console.log('181', articles)
+        expect(articles.length).to.eql(1);
+        done();
+      })
+    })
+
+    // it('should get one article', (done) => {
+    //   console.log('186', user.articleId)
+    //   request('http://127.0.0.1:8080/api/articles/' + user.articleId, (error, response, body) => {
+    //     console.log('187', JSON.parse(body));
+    //     const article = JSON.parse(body);
+    //     expect(article).to.have.key("title");
+    //     done();
+    //   })
+    // })
+
+  // it('should delete one article', (done) => {
+  //   const requestParams = {
+  //     method: 'DELETE',
+  //     uri: 'http://127.0.0.1:8080/api/articles/' + user.articleId,
+  //   }
+  //   request(requestParams, (error, response, body) => {
+  //     console.log('197', body);
+  //     done();
+  //   })
+  // })
+  })
+// after, delete the username, feed, and category
 });
-// // after, delete the username, feed, and category
